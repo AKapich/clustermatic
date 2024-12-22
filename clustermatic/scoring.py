38c6b18be9b8e6492fa1b035ea4ec44842920248
Aleks Kapich <aleks.kapich@gmail.com>
2024-12-23 00:16:42 +0100

    Make code for scoring a bit more concise

--- clustermatic/scoring.py
+++ clustermatic/scoring.py
@@ -5,25 +5,21 @@ from sklearn.metrics import (
 )
 
 
-def silhouette_scorer(estimator, X):
+def cluster_scorer(estimator, X, score_func, default_value):
     labels = estimator.fit_predict(X)
     if len(set(labels)) > 1:
-        return silhouette_score(X, labels)
+        return score_func(X, labels)
     else:
-        return -1
+        return default_value
+
+
+def silhouette_scorer(estimator, X):
+    return cluster_scorer(estimator, X, silhouette_score, -1)
 
 
 def davies_bouldin_scorer(estimator, X):
-    labels = estimator.fit_predict(X)
-    if len(set(labels)) > 1:
-        return davies_bouldin_score(X, labels)
-    else:
-        return float("inf")
+    return cluster_scorer(estimator, X, davies_bouldin_score, float("inf"))
 
 
 def calinski_harabasz_scorer(estimator, X):
-    labels = estimator.fit_predict(X)
-    if len(set(labels)) > 1:
-        return calinski_harabasz_score(X, labels)
-    else:
-        return 0
+    return cluster_scorer(estimator, X, calinski_harabasz_score, 0)
